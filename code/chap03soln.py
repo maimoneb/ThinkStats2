fc4ab41ffc05d64b987edab134b996e31a994d0d
Brian Maimone <bmaimone72@gmail.com>
2016-12-06 14:53:15 -0500

    3-1Solution

--- code/chap03soln.py
+++ code/chap03soln.py
@@ -1,108 +1,57 @@
-"""This file contains code for use with "Think Stats",
-by Allen B. Downey, available from greenteapress.com
 
-Copyright 2014 Allen B. Downey
-License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
-"""
 
-from __future__ import print_function
-
-import numpy as np
-import sys
-
-import nsfg
-import first
 import thinkstats2
 import thinkplot
+import chap01soln
+
+resp = chap01soln.ReadFemResp()
+# print resp.numkdhh
+pmf = thinkstats2.Pmf(resp.numkdhh)
+thinkplot.Pmf(pmf, label='numkdhh')
+thinkplot.Show()
 
 
-def PmfMean(pmf):
-    """Computes the mean of a PMF.
+def BiasPmf(pmf, label):
+    new_pmf = pmf.Copy(label=label)
 
-    Returns:
-        float mean
-    """
-    mean = 0.0
-    for x, p in pmf.d.items():
-        mean += p * x
-    return mean
+    for x, p in pmf.Items():
+        new_pmf.Mult(x, x)
+    new_pmf.Normalize()
+    return new_pmf
 
 
-def PmfVar(pmf, mu=None):
-    """Computes the variance of a PMF.
+
+def BiasPmf(pmf, label=''):
+    """Returns the Pmf with oversampling proportional to value.
+
+    If pmf is the distribution of true values, the result is the
+    distribution that would be seen if values are oversampled in
+    proportion to their values; for example, if you ask students
+    how big their classes are, large classes are oversampled in
+    proportion to their size.
 
     Args:
-        mu: the point around which the variance is computed;
-            if omitted, computes the mean
+      pmf: Pmf object.
+      label: string label for the new Pmf.
 
-    Returns:
-        float variance
+     Returns:
+       Pmf object
     """
-    if mu is None:
-        mu = pmf.Mean()
+    new_pmf = pmf.Copy(label=label)
 
-    var = 0.0
-    for x, p in pmf.d.items():
-        var += p * (x - mu) ** 2
-    return var
+    for x, p in pmf.Items():
+        new_pmf.Mult(x, x)
 
+    new_pmf.Normalize()
+    return new_pmf
 
-def Diffs(t):
-    """List of differences between the first elements and others.
+biased = BiasPmf(pmf, label='biased')
 
-    t: list of numbers
-    
-    returns: list of numbers
-    """
-    first = t[0]
-    rest = t[1:]
-    diffs = [first - x for x in rest]
-    return diffs
+thinkplot.PrePlot(2)
+thinkplot.Pmfs([pmf, biased])
+thinkplot.Show()
 
+print pmf.Mean()
 
-def PairWiseDifferences(live):
-    """Summarize pairwise differences for children of the same mother.
+print biased.Mean()
 
-    live: DataFrame of pregnancy records for live births
-    """
-    live = live[live.prglngth >= 37]
-    preg_map = nsfg.MakePregMap(live)
-
-    diffs = []
-    for caseid, indices in preg_map.items():
-        lengths = live.loc[indices].prglngth.values
-        if len(lengths) >= 2:
-            diffs.extend(Diffs(lengths))
-
-    mean = thinkstats2.Mean(diffs)
-    print('Mean difference between pairs', mean)
-
-    pmf = thinkstats2.Pmf(diffs)
-    thinkplot.Hist(pmf, align='center')
-    thinkplot.Show(xlabel='Difference in weeks',
-                   ylabel='PMF')
-
-
-def main(script):
-    """Tests the functions in this module.
-
-    script: string script name
-    """
-    live, firsts, others = first.MakeFrames()
-    PairWiseDifferences(live)
-
-    # test PmfMean and PmfVar
-    prglngth = live.prglngth
-    pmf = thinkstats2.Pmf(prglngth)
-    mean = PmfMean(pmf)
-    var = PmfVar(pmf)
-
-    assert(mean == pmf.Mean())
-    assert(var == pmf.Var())
-    print('mean/var preg length', mean, var)
-
-    print('%s: All tests passed.' % script)
-
-
-if __name__ == '__main__':
-    main(*sys.argv)
